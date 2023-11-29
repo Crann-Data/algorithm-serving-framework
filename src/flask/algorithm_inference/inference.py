8b629109303726aebfe864b6ee415e3fe315bd51
diarmuidkelly <dok@branchkey.com>
2023-11-29 18:05:20 +0100

    testing sessions threading

--- src/flask/algorithm_inference/inference.py
+++ src/flask/algorithm_inference/inference.py
@@ -12,11 +12,13 @@ class ObjectDetector():
         self.onnx_model = onnx.load(f"{model_directory}/{project_name}.onnx")
         onnx.checker.check_model(self.onnx_model)
 
-        self.ort_session = rt.InferenceSession(f"{model_directory}/{project_name}.onnx", providers=rt.get_available_providers())
+        sess_opt = rt.SessionOptions()
+        sess_opt.intra_op_num_threads = 4
+
+        self.ort_session = rt.InferenceSession(f"{model_directory}/{project_name}.onnx", sess_opt, providers=rt.get_available_providers())
 
     def to_numpy(self, tensor):
         return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
-
     
     def make_prediction(self, image):
         image = Image.open(image)
@@ -32,6 +34,6 @@ class ObjectDetector():
 
         ort_inputs = {self.ort_session.get_inputs()[0].name: image}
         ort_outs = self.ort_session.run(None, ort_inputs)
-        logging.info(f"{datetime.datetime.now() - start}")
+        logging.info(f"{datetime.datetime.now() - start}")        
 
         return True, ort_outs[0].tolist()
