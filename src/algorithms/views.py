7a3a94c55e721a43160fb69db3501ae3872c3d30
diarmuidkelly <26770426+DiarmuidKelly@users.noreply.github.com>
2023-12-07 22:41:51 +0100

    BROKEN: upload object by user

--- src/algorithms/views.py
+++ src/algorithms/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.contrib.auth.models import User
 
 from .models import Algorithm
 from .serializers import AlgorithmSerializer
@@ -23,12 +24,15 @@ class AlgorithmViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows algorithms to be viewed or edited.
     """
-    permission_classes = [IsAuthenticated, IsAdminUser]
-    queryset = Algorithm.objects.all().order_by('-creation_date')
+    permission_classes = [IsAuthenticated]
     serializer_class = AlgorithmSerializer
 
+    def get_queryset(self):
+        return Algorithm.objects.filter(owner_id=self.request.user.id).order_by('-creation_date')
+
+
 def index(request):
-    latest_algorithms_list = Algorithm.objects.order_by("-creation_date")
+    latest_algorithms_list = Algorithm.objects.filter(owner_id=request.user.id).order_by("-creation_date")
     context = {
         "latest_algorithms_list": latest_algorithms_list,
     }
@@ -38,7 +42,10 @@ def get_algo_details(request, algorithm_id):
     try:
         algo = Algorithm.objects.get(pk=algorithm_id)
     except Algorithm.DoesNotExist:
-        raise Http404("Algorithm does not exist")
+        return HttpResponse("algorithm does not exist", status=status.HTTP_404_NOT_FOUND)
+    
+    if not algo.owner.id == request.user.id:
+        return HttpResponse("unauthorised", status=status.HTTP_401_UNAUTHORIZED)
     file = open(algo.algorithm_file.path, mode="rb")
     return FileResponse(file, as_attachment=True, filename=algo.algorithm_file.name)
 
@@ -50,7 +57,7 @@ def upload_algorithm(request):
         if not "file" in request.FILES:
             return Response({"data": "No file given"}, status=status.HTTP_400_BAD_REQUEST)
         file = File(request.FILES["file"])
-        serialized_algo = AlgorithmSerializer(data={"name": file.name, "creation_date":timezone.now(), "algorithm_file":file, "owner": request.user})
+        serialized_algo = AlgorithmSerializer(data={"name": file.name, "creation_date":timezone.now(), "algorithm_file":file, "owner": User.objects.get(id=request.user.id)})
         if serialized_algo.is_valid():
             serialized_algo.save()
             return Response({"data": f"file submitted: {serialized_algo.data}"},status=status.HTTP_201_CREATED)
